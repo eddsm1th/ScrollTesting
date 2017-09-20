35310b357acbea340fa8463c145108088dc35366
Edd <smith.edward@hotmail.com>
2017-09-20 20:54:23 +0100

    reworked stucture

---
+++ assets/js/Main.js
@@ -0,0 +1,38 @@
+var current_slide;
+
+function scroll_up() { 
+    if ( current_slide < $('.slide').length)  {
+        $('.slide-' + current_slide.toString()).css('margin-top','-100%');
+        $('.slide-' + current_slide.toString()).delay(1600).fadeOut(1);
+
+        current_slide ++;
+    }
+    console.log(current_slide);
+}
+
+function scroll_down(){
+    $('.slide-' + (current_slide - 1).toString()).fadeIn(1);
+    $('.slide-' + (current_slide - 1).toString()).css('margin-top','0px');
+
+    if ( current_slide > 0)  {
+        current_slide --;
+    }
+    console.log(current_slide);
+}
+
+function define_var(){
+    current_slide = 1;
+}
+
+function move(){
+    var Xoffset = get_offset(event.clientX, $( window ).width())
+    var Yoffset = get_offset(event.clientY, $( window ).height())
+
+    console.log('X:' + Xoffset + ' Y:' + Yoffset);
+    $('.move-image').css('transform','translate(' + Xoffset + 'px, ' + Yoffset + 'px) scale(1.04)');
+    $('.content-wrap').css('transform','translate(' + Xoffset*2 + 'px, ' + Yoffset*2 + 'px)');
+}
+
+function get_offset(mouse, browser){
+    return (((mouse - (browser / 2))/(browser / 2))*10) * -1;
+}
