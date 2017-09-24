568afbcad74300b2ba565e81b8b076606ccdedec
Edd <smith.edward@hotmail.com>
2017-09-24 20:15:32 +0100

    a bunch of stuff

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -3,21 +3,44 @@ var current_slide;
 function scroll_up() { 
     if ( current_slide < $('.slide').length)  {
         $('.slide-' + current_slide.toString()).css('margin-top','-100%');
-        $('.slide-' + current_slide.toString()).delay(1000).fadeOut(1);
+        
+        if ( current_slide == 1 ) {
+            $('.slide-' + current_slide.toString()).delay(1000).fadeOut(1);
+        }
+
+        $('.slide-' + (current_slide + 1).toString()).fadeIn(1);
+        // $('.slide-' + (current_slide - 1).toString() + ' > .content-wrap').fadeIn(1);
 
         current_slide ++;
     }
+
+    get_arrow_colour();
+
     console.log(current_slide);
 }
 
 function scroll_down(){
     $('.slide-' + (current_slide - 1).toString()).fadeIn(1);
+    // $('.slide-' + (current_slide - 1).toString() + ' > .content-wrap').fadeIn(1);
     $('.slide-' + (current_slide - 1).toString()).css('margin-top','0px');
+    
+    if( current_slide != 1 ) {
+        $('.slide-' + current_slide.toString()).delay(1600).fadeOut(1);    
+    }
 
-    if ( current_slide > 0)  {
+    if ( current_slide > 1)  {
         current_slide --;
     }
+
+    get_arrow_colour();
+
     console.log(current_slide);
+}
+
+function get_arrow_colour(){
+    var arrow_colours = ["white","gold"];
+
+    $('.scroll-arrows').css('color',arrow_colours[current_slide - 1]);
 }
 
 function define_var(){
@@ -29,7 +52,7 @@ function move(){
     var Yoffset = get_offset(event.clientY, $( window ).height())
 
     console.log('X:' + Xoffset + ' Y:' + Yoffset);
-    $('.move-image, .slide-1-text, .diamond-wrap').css('transform','translate(' + Xoffset + 'px, ' + Yoffset + 'px) scale(1.04)');
+    $('.move-image, .slide-1-text').css('transform','translate(' + Xoffset + 'px, ' + Yoffset + 'px) scale(1.04)');
     $('.content-wrap').css('transform','translate(' + Xoffset*2 + 'px, ' + Yoffset*2 + 'px)');
 }
 
